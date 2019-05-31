0436371d275d52b53e76d98ffd4dd389453dd218
Piotr Konopnicki <pkonopnicki@liquidadvertising.com>
2019-05-31 09:46:31 +0200

    Added functionality: - dictionary with buymodel/buyrate - dictionary with start/end date - output generates additional columns with buymodel and buyrate - possibility to change buymodel/buyrate for each vendor - if no buymodel/buyrate was added, this vendor will have null as buymodel and buyrate

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ output_file = 'output.xlsx'
 
 df = pd.read_excel(input_file)
 
+#Creating combinations of the input variables
 first_col = df.columns[0]
 first_col_unique = df[first_col].unique()
 
@@ -22,5 +23,22 @@ for val in first_col_unique:
     output_df = output_df.append(df1, ignore_index=True)
 
 #Insert additional empty column if needed
-#output_df.insert(1, 'PKPartnerName', '')
-output_df.to_excel(output_file, index=False)
+#output_df.insert(1,'PKPartnerName', '')
+
+#Adding buy model based on a dictionary
+
+input_file_buymodel_dictionary = 'dictionary/buymodel.xlsx'
+df_buymodel_dictionary = pd.read_excel(input_file_buymodel_dictionary)
+
+output_df['Vendor_left'] = output_df['Vendor']
+output_df['Vendor_left'] = output_df['Vendor_left'].str.lower()
+output_df['Vendor_left'] = output_df['Vendor_left'].str.replace(' ','')
+
+df_buymodel_dictionary['Vendor_right'] = df_buymodel_dictionary['Vendor']
+df_buymodel_dictionary['Vendor_right'] = df_buymodel_dictionary['Vendor_right'].str.lower()
+df_buymodel_dictionary['Vendor_right'] = df_buymodel_dictionary['Vendor_right'].str.replace(' ','')
+
+merge_df = pd.merge(output_df,df_buymodel_dictionary, left_on='Vendor_left', right_on='Vendor_right', suffixes=('','_right_join'), how='left')
+merge_df = merge_df.drop(columns = ['Vendor_left','Vendor_right_join','Vendor_right'])
+
+merge_df.to_excel(output_file, index=False)
